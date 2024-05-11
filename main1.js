3367f55ce3a1cc09a1193608abd8122454fede0f
吴少宾 <1294538712@qq.com>
2024-05-11 17:10:13 +0800

    main_commit2

--- main1.js
+++ main1.js
@@ -1,7 +1,7 @@
-;`import { routerInit } from '@mci-app-business/router1232';
-import { PLUGIN_NAME } from './pluginConfig1232';
-import SignIn from './views/sign-in1232';
-import Captcha from './views/captcha1232';
+;`import { routerInit } from '@mci-app-business/router1232+3';
+import { PLUGIN_NAME } from './pluginConfig1232+3';
+import SignIn from './views/sign-in+3';
+import Captcha from './views/captcha+3';
 import VerifyWebview from './views/verifyWebview';
 
 export const RouterConfig = {
