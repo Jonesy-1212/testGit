f3644f046b55735314db9ceefc8e41ed8680b194
吴少宾 <1294538712@qq.com>
2024-05-11 16:03:16 +0800

    xx

---
+++ main2.js
@@ -0,0 +1,28 @@
+;`{
+  "name": "@mci-app-plugin/local-account",
+  "version": "1.0.0",
+  "type": "module",
+  "main": "./src/index.tsx",
+  "types": "./src/index.tsx",
+  "devDependencies": {
+    "@mci-app/typescript-config": "workspace:*",
+    "@types/react": "18.2.48",
+    "typescript": "5.3.3"
+  },
+  "dependencies": {
+    "@mci-app-business/router": "workspace:*",
+    "@mci-app-business/ui": "workspace:*",
+    "@mci-app-business/hooks": "workspace:*",
+    "@mci-app/ui": "workspace:*",
+    "@react-native-community/clipboard": "1.5.1",
+    "openinstall-react-native": "^1.7.3",
+    "query-string": "8.1.0",
+    "react-native-config": "1.5.1",
+    "react-native-webview": "13.2.2"
+  },
+  "peerDependencies": {
+    "react": "18.2.0",
+    "react-native": "0.72.6"
+  }
+}
+`
